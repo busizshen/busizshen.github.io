0d0725c8023366165bfcd716690dd577dd24b035
wanghj <78732833@qq.com>
2018-11-12 13:14:43 +0800

    test script

--- oscommand.py
+++ oscommand.py
@@ -1,10 +1,10 @@
 import os
 
-
+print(1)
 print(os.system('git add .'))
-
+print(2)
 print(os.system('git pull origin  master'))
-
+print(3)
 print(os.system('git commit -m "test script"'))
-
+print(4)
 print(os.system('git push -u origin master'))
